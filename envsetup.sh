4cdc359619b6f75f98be7728351eb627d2d02ced
Jayanta Ghoshal <jayanta.ghosal@ecarxgroup.com>
2023-09-22 01:00:30 +0200

    add envsetup

---
+++ envsetup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export VENDOR_HOME=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export VENDOR_PACKAGE="$VENDOR_HOME"/packages
+
+export EROOT="$(dirname "$VENDOR_HOME")"
+export ESP_IDF_HOME="$EROOT"/esp-idf
+
+
+
+echo -e "EROOT\t\t:\t$EROOT"
+echo -e "ESP_IDF_HOME\t:\t$ESP_IDF_HOME"
+echo -e "VENDOR_HOME\t:\t$VENDOR_HOME"
+echo -e "VENDOR_PACKAGE\t:\t$VENDOR_PACKAGE"
+
+
+source $ESP_IDF_HOME/export.sh
+
--- export.sh
+++ export.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-MY_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export VENDOR_HOME=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export VENDOR_PACKAGE="$VENDOR_HOME"/packages
 
-export VENDOR_PACKAGE="$MY_PATH"/packages
+
