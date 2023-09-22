8991a33840159782304b295bc2e4926699d1b29e
Jayanta Ghoshal <jayanta.ghosal@ecarxgroup.com>
2023-09-23 00:11:25 +0200

    all module building

--- packages/ble_onoff_client/main/main.c
+++ packages/ble_onoff_client/main/main.c
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <inttypes.h>
 
 #include "esp_log.h"
 #include "nvs_flash.h"
@@ -120,7 +121,7 @@ static void mesh_example_info_restore(void)
 static void prov_complete(uint16_t net_idx, uint16_t addr, uint8_t flags, uint32_t iv_index)
 {
     ESP_LOGI(TAG, "net_idx: 0x%04x, addr: 0x%04x", net_idx, addr);
-    ESP_LOGI(TAG, "flags: 0x%02x, iv_index: 0x%08x", flags, iv_index);
+    ESP_LOGI(TAG, "flags: 0x%02x, iv_index: 0x%08" PRIx32, flags, iv_index);
     gpio_operation(LED_G, LED_OFF);
     store.net_idx = net_idx;
     /* mesh_example_info_store() shall not be invoked here, because if the device
@@ -180,9 +181,7 @@ void example_ble_mesh_send_gen_onoff_set(void)
     common.ctx.app_idx = store.app_idx;
     common.ctx.addr = 0xFFFF;   /* to all nodes */
     common.ctx.send_ttl = 3;
-    common.ctx.send_rel = false;
     common.msg_timeout = 0;     /* 0 indicates that timeout value from menuconfig will be used */
-    common.msg_role = ROLE_NODE;
 
     set.onoff_set.op_en = false;
     set.onoff_set.onoff = store.onoff;
@@ -201,7 +200,7 @@ void example_ble_mesh_send_gen_onoff_set(void)
 static void example_ble_mesh_generic_client_cb(esp_ble_mesh_generic_client_cb_event_t event,
                                                esp_ble_mesh_generic_client_cb_param_t *param)
 {
-    ESP_LOGI(TAG, "Generic client, event %u, error code %d, opcode is 0x%04x",
+    ESP_LOGI(TAG, "Generic client, event %u, error code %d, opcode is 0x%04" PRIx32,
         event, param->error_code, param->params->opcode);
 
     switch (event) {
