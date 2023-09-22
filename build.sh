1c54f14d7009e2a189e5f490efc111bbdec95bb7
Jayanta Ghoshal <jayanta.ghosal@ecarxgroup.com>
2023-09-22 02:22:59 +0200

    build.sh -list

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 if [ "$1" = "-h" ];then
     echo "source build.sh [package_name] [--clean]"
     echo "source build.sh -h"
+    echo "source build.sh -list"
     return 0
 fi
 
@@ -15,12 +16,20 @@ MY_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 package_array=()
 package_array_string=$(ls $VENDOR_PACKAGE)
 package_array=($package_array_string)
+
+if [ "$1" = "-list" ];then
+    for package in "${package_array[@]}"
+    do
+        echo "$package"
+    done
+return 0
+fi
+
 package_path_array=()
 for package in "${package_array[@]}"
 do
   package_path_array+=("$VENDOR_PACKAGE/$package")
 done
-echo ${package_path_array[@]}
 echo "$1"
 # run idf.py build
 requested_package_path=("$VENDOR_PACKAGE/$1")
