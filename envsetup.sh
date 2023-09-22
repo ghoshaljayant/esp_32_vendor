dd80dd523d93a90353db64b114dbb3748ccfbddb
Jayanta Ghoshal <jayanta.ghosal@ecarxgroup.com>
2023-09-22 23:07:13 +0200

    envsetup supporting symbolic link

--- envsetup.sh
+++ envsetup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-export VENDOR_HOME=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+export VENDOR_HOME=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
 export VENDOR_PACKAGE="$VENDOR_HOME"/packages
 
 export EROOT="$(dirname "$VENDOR_HOME")"
@@ -8,8 +16,10 @@ export ESP_IDF_HOME="$EROOT"/esp-idf
 export IDF_BUILD_TARGET="esp32"
 
 
+alias eroot="cd $EROOT"
 alias build.sh="source $VENDOR_HOME/build.sh"
 alias build_all.sh="source $VENDOR_HOME/build_all.sh"
+
 [[ ! -d $ESP_IDF_HOME ]] && cd $EROOT && git clone https://github.com/espressif/esp-idf.git && bash $ESP_IDF_HOME/install.sh
 
 echo "Running $ESP_IDF_HOME/export.sh ..."
