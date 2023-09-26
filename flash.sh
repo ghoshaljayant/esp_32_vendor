43591e96bdb02f51e4a9777fe8964f3ee2d88884
Jayanta Ghoshal <jayanta.ghosal@ecarxgroup.com>
2023-09-26 02:26:09 +0200

    New changes

--- flash.sh
+++ flash.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-if [ "$1" = "-h" ];then
-    echo "source flash.sh [package_name]"
-    echo "source flash.sh -h"
-    echo "source flash.sh -list"
+args=$#
+function print_help()
+{
+  echo
+  echo "Usage:"
+  echo
+  echo " - source flash.sh [package_name]"
+  echo " - source flash.sh -h"
+  echo " - source flash.sh -list"
+  echo
+}
+
+if [ $args = 0 ] || [ $1 = "-h" ];then
+    print_help
     return 0
 fi
 
-MY_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+export MY_PATH=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+source $MY_PATH/tools.sh
+
 # check if ${VENODR_PACKAGE} is exported
 # check if ${VENODR_PACKAGE} path exists
-[[ ! -d $VENDOR_PACKAGE ]] && "ERROR: Please run esp_32_vendor/envsetup.sh" && return -1
-[[ -n $DEVICE_PORT ]] && "ERROR: Please run esp_32_vendor/envsetup.sh" && return -1
+[[ ! -d $VENDOR_PACKAGE ]] && "ERROR: VENDOR_PACKAGE Please run esp_32_vendor/envsetup.sh" && exit
+[[ ! -n $DEVICE_PORT ]] && "ERROR: DEVICE_PORT Please run esp_32_vendor/envsetup.sh" && exit
+
+
+detect_device
 
 # list all pacakges under ${VENODR_PACKAGE}
 package_array=()
@@ -33,6 +56,14 @@ do
 done
 echo "$1"
 # run idf.py -p $DEVICE_PORT flash
+
+if [ "$ESP_DEVICE_COUNT" = "0" ];then
+  echo "No device connected !!"
+  return
+fi
+
+
+
 requested_package_path=("$VENDOR_PACKAGE/$1")
 for package_path in "${package_path_array[@]}"
 do
@@ -40,9 +71,17 @@ do
   if [ "$requested_package_path" = "$package_path" ];then
 
     cd $package_path
-    echo ">> executing: idf.py -p $DEVICE_PORT flash"
-    idf.py -p $DEVICE_PORT flash
+    if [ "$ESP_DEVICE_COUNT" = "1" ];then      
+      echo ">> executing: idf.py -p $DEVICE_PORT flash"
+      idf.py -p $DEVICE_PORT flash
+    else
+
+      list_device_port_by_index
+      read -p 'enter the choice: ' device_port_index
+      device_port=$(get_device_port_by_index $device_port_index)
+      echo ">> executing: idf.py -p $device_port flash"
+      idf.py -p $device_port flash
+    fi
     cd -
-    
   fi
 done
---
+++ tools.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+result="FAIL"
+
+function detect_device()
+{
+    export ESP_DEVICES="NULL"
+    device_port_list=$(ls /dev |grep ttyACM)
+
+    device_index=0
+    if [ "$device_port_list" != "" ];then
+        while IFS= read -r device_port ; do        
+            eval "export DEVICE_PORT_$device_index=/dev/$device_port"
+            (( device_index++ ))  
+        done <<< "$device_port_list"
+    fi
+
+    case $device_index in
+    0)  export ESP_DEVICE_STATUS="NULL"
+        ;;
+    1)  export ESP_DEVICE_STATUS="SINGLE_DEVICE_CONNECTED"
+        ;;
+    *)  export ESP_DEVICE_STATUS="MULTI_DEVICE_CONNECTED"
+        ;;
+    esac
+
+    export ESP_DEVICE_COUNT="$device_index"
+}
+
+function list_device_port_by_index()
+{
+    device_port_list=$(ls /dev |grep ttyACM)
+
+    device_index=0
+    if [ "$device_port_list" != "" ];then
+        while IFS= read -r device_port ; do        
+            echo "$device_index. $device_port"
+            (( device_index++ ))  
+        done <<< "$device_port_list"
+    fi
+}
+
+function get_device_port_by_index()
+{
+    device_port_list=$(ls /dev |grep ttyACM)
+    requested_index=$1
+    device_index=0
+    if [ "$device_port_list" != "" ];then
+        while IFS= read -r device_port ; do
+            if [ "$device_index" = "$requested_index" ];then
+                echo "/dev/$device_port"
+            fi            
+            (( device_index++ ))  
+        done <<< "$device_port_list"
+    fi
+}
