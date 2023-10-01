b00084b6d7cb2416a15f8e9a7c779ab7e87a1923
Jayanta Ghoshal <jayanta.ghosal@ecarxgroup.com>
2023-10-01 12:39:10 +0200

    New changes

--- scripts/csv_flaher.sh
+++ scripts/csv_flaher.sh
@@ -5,10 +5,29 @@ if [ "$1" = "-h" ];then
   return 0
 fi
 
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+export MY_PATH=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+
+source $MY_PATH/tools.sh
+detect_device
+if [ "$ESP_DEVICE_COUNT" = "0" ];then
+  echo "No device connected !!"
+  return -1
+fi
+
+index=0
 while IFS="," read -r path package_name
 do
-  echo "$path"
-  echo "$package_name"
-  echo ""
-  flash.sh "$path" "$package_name"
+  path=$(eval echo "$path")
+  device_port=$(get_device_port_by_index $index)
+  cd $path
+  idf.py -p $device_port flash
+  cd -
+  (( index++ ))   
 done < <(tail -n +2 $1)
